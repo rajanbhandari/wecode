c174a83ad57fa304b9da706f54c381ca4732ed39
rajanbhandari <34101537+rajanbhandari@users.noreply.github.com>
2018-01-26 14:05:23 +0545

    Add files via upload

---
+++ aftersearch.php
@@ -0,0 +1,115 @@
+<?php require_once ('public/lib/includes/header.php'); ?>
+<br><br>
+
+<div class="container panel-edit">
+  <h2 class="header-search-fi">Nearby Restaurants Located </h2>
+  <div class="dropdown ">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+   Filtered By....
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Favourite</a>
+    <a class="dropdown-item" href="#">Cheap Price</a>
+    
+  </div>
+</div>
+<br>
+  <div class="card">
+    <div class="card-body">
+    	<div class="row">
+    		<div class="col-md-5">
+				      <h4 class="card-title"><span><i class="fa fa-map-marker ico-ed" aria-hidden="true"></i></span>&nbsp;&nbsp;Black Forest</h4>
+				      <p class="card-text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chaura,Butwal</p>
+				        <p class="card-text"><span><i class="fa fa-clock-o ico-ed" aria-hidden="true"></i></span>&nbsp;&nbsp;Open : 10 A.M - 8 P.M</p>
+				        <p class="card-text">Delivery Distace : 10 KM </p>
+				        <p class="card-text">Minimum Order : Rs 300/-</p>
+				     
+          </div>
+          <div class="col-md-4">
+          	<h4 style="color:green">Customers Rate</h4>
+             <p><img  class="rating-size" src="public/img/ratingblackforest.png"/></p>
+          </div>
+          <div class="col-md-3">
+              <center><button type="button" class="btn btn-primary">Order From This</button></center>
+          </div>
+    </div>
+</div>
+</div>
+<div class="card">
+    <div class="card-body">
+    	<div class="row">
+    		<div class="col-md-5">
+				      <h4 class="card-title"><span><i class="fa fa-map-marker ico-ed" aria-hidden="true"></i></span>&nbsp;&nbsp;Black Forest</h4>
+				      <p class="card-text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chaura,Butwal</p>
+				        <p class="card-text"><span><i class="fa fa-clock-o ico-ed" aria-hidden="true"></i></span>&nbsp;&nbsp;Open : 10 A.M - 8 P.M</p>
+				        <p class="card-text">Delivery Distace : 10 KM </p>
+				        <p class="card-text">Minimum Order : Rs 300/-</p>
+				     
+          </div>
+          <div class="col-md-4">
+          	<h4 style="color:green">Customers Rate</h4>
+             <p><img  class="rating-size" src="public/img/ratingblackforest.png"/></p>
+          </div>
+          <div class="col-md-3">
+              <center><button type="button" class="btn btn-primary">Order From This</button></center>
+          </div>
+    </div>
+</div>
+</div>
+<div class="card">
+    <div class="card-body">
+    	<div class="row">
+    		<div class="col-md-5">
+				      <h4 class="card-title"><span><i class="fa fa-map-marker ico-ed" aria-hidden="true"></i></span>&nbsp;&nbsp;Black Forest</h4>
+				      <p class="card-text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chaura,Butwal</p>
+				        <p class="card-text"><span><i class="fa fa-clock-o ico-ed" aria-hidden="true"></i></span>&nbsp;&nbsp;Open : 10 A.M - 8 P.M</p>
+				        <p class="card-text">Delivery Distace : 10 KM </p>
+				        <p class="card-text">Minimum Order : Rs 300/-</p>
+				     
+          </div>
+          <div class="col-md-4">
+          	<h4 style="color:green">Customers Rate</h4>
+             <p><img  class="rating-size" src="public/img/ratingblackforest.png"/></p>
+          </div>
+          <div class="col-md-3">
+            <center><button type="button" class="btn btn-primary">Order From This</button></center>
+          </div>
+    </div>
+</div>
+</div>	
+<div class="card">
+    <div class="card-body">
+    	<div class="row">
+    		<div class="col-md-5">
+				      <h4 class="card-title"><span><i class="fa fa-map-marker ico-ed" aria-hidden="true"></i></span>&nbsp;&nbsp;Black Forest</h4>
+				      <p class="card-text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chaura,Butwal</p>
+				        <p class="card-text"><span><i class="fa fa-clock-o ico-ed" aria-hidden="true"></i></span>&nbsp;&nbsp;Open : 10 A.M - 8 P.M</p>
+				        <p class="card-text">Delivery Distace : 10 KM </p>
+				        <p class="card-text">Minimum Order : Rs 300/-</p>
+				     
+          </div>
+          <div class="col-md-4">
+          	<h4 style="color:green">Customers Rate</h4>
+             <p><img  class="rating-size" src="public/img/ratingblackforest.png"/></p>
+          </div>
+          <div class="col-md-3">
+             <center><button type="button" class="btn btn-primary">Order From This</button></center>
+          </div>
+    </div>
+</div>
+</div>			
+</div>
+
+
+</div>
+	
+
+    <?php require_once ('public/lib/includes/login.php'); ?>
+    <?php require_once ('public/lib/includes/signup.php'); ?>
+    <?php require_once ('public/lib/includes/help.php'); ?>
+    <?php require_once ('public/lib/includes/forgetpw.php'); ?>
+
+<?php require_once ('public/lib/includes/onlineprocess.php'); ?>
+
+
+<?php require_once ('public/lib/includes/footer.php'); ?>
